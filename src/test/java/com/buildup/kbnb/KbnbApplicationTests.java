5aca06980f3f8cf9f0986dd58ffdc1959f04a0c0
Kimyechan <vlvkcjswo7@naver.com>
2021-02-01 14:48:01 +0900

    Conf: Change ContextLoad To Test Config

--- src/test/java/com/buildup/kbnb/KbnbApplicationTests.java
+++ src/test/java/com/buildup/kbnb/KbnbApplicationTests.java
@@ -2,8 +2,10 @@ package com.buildup.kbnb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KbnbApplicationTests {
 
     @Test
