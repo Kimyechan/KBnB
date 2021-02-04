cdaea8237915a291f87e42e6ea3180d2fc95dc9d
Hansol-Jeong <shn03014@naver.com>
2021-02-04 18:57:53 +0900

    refactor: refactor ReservationControllerTest
    register_Reservation()
    Probelm was the AutoIncrease config which start from 2 instead 1.
    Solution: use Mock.

--- src/main/java/com/buildup/kbnb/model/Reservation.java
+++ src/main/java/com/buildup/kbnb/model/Reservation.java
@@ -24,9 +24,9 @@ public class Reservation {
 
     private Double totalCost;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY )
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY )
     private Room room;
 }
