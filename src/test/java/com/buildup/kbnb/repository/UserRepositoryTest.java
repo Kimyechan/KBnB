cf0010f27737574fb11a33f385fb4aef997620a8
Kimyechan <vlvkcjswo7@naver.com>
2021-02-11 15:04:47 +0900

    Test: Get Check Room List
    solved #64
    Persistence Test
    Checking lazy loading fetch join
    Using DistiesContext partly

--- src/test/java/com/buildup/kbnb/repository/UserRepositoryTest.java
+++ src/test/java/com/buildup/kbnb/repository/UserRepositoryTest.java
@@ -4,18 +4,22 @@ import com.buildup.kbnb.model.UserRoom;
 import com.buildup.kbnb.model.room.Room;
 import com.buildup.kbnb.model.user.AuthProvider;
 import com.buildup.kbnb.model.user.User;
+import org.hibernate.Hibernate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class UserRepositoryTest {
     @Autowired
     TestEntityManager entityManager;
@@ -66,6 +70,6 @@ class UserRepositoryTest {
         User user = userRepository.findByIdWithCheckRoom(1L).orElse(null);
 
         assert user != null;
-        assertThat(user.getCheckRoomList()).isNotEmpty();
+        assertTrue(Hibernate.isInitialized(user.getCheckRoomList()));
     }
 }
--- src/test/java/com/buildup/kbnb/repository/UserRoomRepositoryTest.java
+++ src/test/java/com/buildup/kbnb/repository/UserRoomRepositoryTest.java
@@ -1,7 +1,64 @@
 package com.buildup.kbnb.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.buildup.kbnb.model.UserRoom;
+import com.buildup.kbnb.model.room.Room;
+import com.buildup.kbnb.model.user.AuthProvider;
+import com.buildup.kbnb.model.user.User;
+import org.hibernate.Hibernate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@ActiveProfiles("test")
 class UserRoomRepositoryTest {
+    @Autowired
+    TestEntityManager entityManager;
 
+    @Autowired
+    UserRoomRepository userRoomRepository;
+
+    User savedUser;
+    @BeforeEach
+    public void setUp() {
+        User user = User.builder()
+                .name("test")
+                .email("test@gmail.com")
+                .emailVerified(false)
+                .provider(AuthProvider.local)
+                .build();
+        savedUser = entityManager.persist(user);
+
+        Room room = Room.builder()
+                .name("test room")
+                .build();
+        entityManager.persist(room);
+
+        UserRoom userRoom = UserRoom.builder()
+                .user(user)
+                .room(room)
+                .build();
+        entityManager.persist(userRoom);
+
+        entityManager.flush();
+        entityManager.clear();
+    }
+    @Test
+    @DisplayName("유저 식별자로 찜한 방 리스트 찾기")
+    public void findRoomListByUserId() {
+        List<UserRoom> userRoomList = userRoomRepository.findByUserId(savedUser.getId());
+
+        assertThat(userRoomList.size()).isEqualTo(1);
+        assertTrue(Hibernate.isInitialized(userRoomList.get(0).getRoom()));
+        assertTrue(Hibernate.isInitialized(userRoomList.get(0).getUser()));
+    }
 }
