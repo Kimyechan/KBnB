87741496377f6df46e567b6223f214e46f79ffc7
Kimyechan <vlvkcjswo7@gmail.com>
2021-02-24 13:35:34 +0900

    Update deploy.sh
    run background

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ fi
 
 echo "> $JAR_PATH 배포"
 # nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
-nohup java -jar $JAR_PATH >/home/ubuntu/kbnblog.out 2>&1
+nohup java -jar $JAR_PATH >/home/ubuntu/kbnblog.out 2>&1 &
